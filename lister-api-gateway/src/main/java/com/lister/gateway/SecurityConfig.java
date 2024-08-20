f01cd0402e5604e194aa4ea82645f06891090c7b
Danil Maksimov <danil.maksimov@stringdata.cz>
2024-08-20 20:55:48 +0200

    Finished security setup

--- lister-api-gateway/src/main/java/com/lister/gateway/SecurityConfig.java
+++ lister-api-gateway/src/main/java/com/lister/gateway/SecurityConfig.java
@@ -2,42 +2,24 @@ package com.lister.gateway;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.Arrays;
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .cors()
-                .and()
-                .authorizeHttpRequests(authorizeRequests ->
-                        authorizeRequests.anyRequest().authenticated())
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
-
-        return http.build();
-    }
-
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(Arrays.asList("*")); // Allow all origin patterns (e.g. http://example.com
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(Arrays.asList("*")); // Allow all headers
-        configuration.setAllowCredentials(true); // Allow credentials if needed
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration); // Apply CORS to all paths
-        return source;
+        return http
+                .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers("/test").authenticated()
+                )
+                .cors(withDefaults())
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(withDefaults())
+                )
+                .build();
     }
 }
--- lister-api-gateway/src/main/java/com/lister/gateway/TestController.java
+++ lister-api-gateway/src/main/java/com/lister/gateway/TestController.java
@@ -1,13 +1,16 @@
 package com.lister.gateway;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@CrossOrigin(origins = "*")
 public class TestController {
 
     @GetMapping("/test")
-    public String test() {
+    public @ResponseBody String test() {
         return "Мы молодцы!";
     }
 }
